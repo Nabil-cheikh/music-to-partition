a28179854e3331d0fe074378eb94f10325153634
Nabil Cheikh <nabil.cheikh13@hotmail.com>
2025-02-28 11:36:47 +0100

    Instanciation of my app with Vue js

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Music To Partition</h1>
+    <p>Upload the music you want to convert into a partition here :</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
